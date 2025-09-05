41b53bd69fb78435bcd4bf6c7019723781a9a354
leandrapalmeiras <leandra.pereira.cruz@escola.pr.gov.br>
2025-09-05 09:04:27 -0300

    onclick

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 const numeroSenha = document.querySelector('parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha = tamanhoSenha
+
+const botoes = document.querySelectorAll('parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) {
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanhoTamanho(){
+    if (tamanhoSenha > 20) {
+        //tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
